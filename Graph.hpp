ebe1c33d40af67feff9cb2046be0d919e6b405d6
AVAtarMod <Respawner289@gmail.com>
2023-04-11 12:34:12 +0300

    Fix build

--- Graph.hpp
+++ Graph.hpp
@@ -4,8 +4,7 @@
 //! The quality of the code in this library is so low (it's very complex and hard to read)
 
 #include <vector>
-
-#include "list.hpp"
+#include <iostream>
 
 using matrix_t = std::vector<std::vector<int>>;
 
@@ -14,18 +13,18 @@ class Graph {
     uint size = 0;
 
     // * Must be List1D<int>* adjacencyList
-    List1D<List1D<int>> adjacencyList = nullptr;
+   //  List1D<List1D<int>> adjacencyList = nullptr;
 
   public:
     enum Method { BY_DEPTH, BY_WIDTH };
 
     Graph();
     Graph(const matrix_t& adjacencyMatrix);
-    Graph(const List1D<List1D<int>> adjacencyList);
+   //  Graph(const List1D<List1D<int>> adjacencyList);
 
     void operator=(const Graph& graph);
 
-    List1D<List1D<int>> getAdjacencyList() const;
+   //  List1D<List1D<int>> getAdjacencyList() const;
     const matrix_t getAdjacencyMatrix() const;
     Graph getSpanningTree(Method m) const;
     void print() const;
